990e59dfdd8293abcaca5986a4abfc39226c084b
Ben (MBA M2) <ben@wolkenwelten.net>
2024-09-05 15:02:31 +0900

    WIP: entity system

--- src/backend/db/migrations/index.ts
+++ src/backend/db/migrations/index.ts
@@ -1,6 +1,6 @@
 export const initSQLTables = `
 CREATE TABLE IF NOT EXISTS content (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id INTEGER PRIMARY KEY,
 	createdAt INTEGER,
 
 	uri TEXT,
@@ -10,7 +10,7 @@ CREATE INDEX content_uri ON content (uri);
 
 
 CREATE TABLE IF NOT EXISTS revision (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id INTEGER PRIMARY KEY,
 	createdAt INTEGER,
 	previousRevision INTEGER,
 
@@ -22,7 +22,7 @@ CREATE INDEX revision_previousRevision ON revision (previousRevision);
 
 
 CREATE TABLE IF NOT EXISTS resource (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id INTEGER PRIMARY KEY,
 	createdAt INTEGER,
 
 	path TEXT,
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS session (
 
 
 CREATE TABLE IF NOT EXISTS user (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id INTEGER PRIMARY KEY,
 	createdAt INTEGER,
 	privilegeLevel TEXT,
 	email TEXT,
@@ -54,7 +54,7 @@ CREATE INDEX user_email ON user (email);
 
 
 CREATE TABLE IF NOT EXISTS userKVEntry (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id INTEGER PRIMARY KEY,
 	user INTEGER,
 	key TEXT,
 	createdAt INTEGER,
@@ -64,4 +64,60 @@ CREATE TABLE IF NOT EXISTS userKVEntry (
 CREATE INDEX userKVEntry_createdAt ON userKVEntry (createdAt);
 CREATE INDEX userKVEntry_user ON userKVEntry (user);
 CREATE INDEX userKVEntry_user_key ON userKVEntry (user, key);
+
+
+CREATE TABLE IF NOT EXISTS entity (
+	id INTEGER PRIMARY KEY,
+	createdAt INTEGER,
+	type TEXT,
+	parent INTEGER DEFAULT 0,
+	creator INTEGER DEFAULT 0,
+	permission INTEGER DEFAULT 0,
+	uri TEXT DEFAULT ""
+);
+CREATE INDEX entity_parent ON entity (parent);
+CREATE INDEX entity_uri ON entity (uri);
+CREATE INDEX entity_parent_type ON entity (parent, type);
+CREATE INDEX entity_creator ON entity (creator);
+
+CREATE TABLE IF NOT EXISTS entityPermission (
+	id INTEGER PRIMARY KEY,
+	createdAt INTEGER,
+	level INTEGER DEFAULT 0,
+	member INTEGER,
+	entity INTEGER,
+
+	CONSTRAINT fk_member
+		FOREIGN KEY (member)
+		REFERENCES entity(id)
+		ON DELETE CASCADE,
+
+	CONSTRAINT fk_entity
+		FOREIGN KEY (entity)
+		REFERENCES entity(id)
+		ON DELETE CASCADE
+);
+CREATE INDEX entityPermission_member_entity ON entityPermission (member, entity);
+CREATE INDEX entityPermission_member ON entityPermission (member);
+CREATE INDEX entityPermission_entity ON entityPermission (entity);
+
+CREATE TABLE IF NOT EXISTS entityValue (
+	id INTEGER PRIMARY KEY,
+	createdAt INTEGER,
+	creator INTEGER,
+	entity INTEGER,
+	key TEXT,
+	value TEXT,
+	permission INTEGER DEFAULT 0,
+	previousVersion INTEGER DEFAULT 0,
+
+	CONSTRAINT fk_entity
+		FOREIGN KEY (entity)
+		REFERENCES entity(id)
+		ON DELETE CASCADE
+);
+CREATE INDEX entityValue_entity ON entityValue (entity);
+CREATE INDEX entityValue_entity_key ON entityValue (entity, key);
+CREATE INDEX entityValue_entity_creator ON entityValue (entity, creator);
+
 `;
